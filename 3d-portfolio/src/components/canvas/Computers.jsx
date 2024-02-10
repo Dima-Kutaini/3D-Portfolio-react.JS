457ca43f59c2639ab951fc626c57f5f7c2f8e02f
Dima-Kutaini <deemaKutainy@gmail.com>
2024-02-10 21:46:43 +0100

    create Computers and Hero components

--- 3d-portfolio/src/components/canvas/Computers.jsx
+++ 3d-portfolio/src/components/canvas/Computers.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
+/** @format */
 
+import React, { Suspense } from 'react';
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls, Preload, useGLTF } from '@react-three/drei';
+import CanvasLoader from '../Loader';
 const Computers = () => {
-  return (
-    <div>Computers</div>
-  )
-}
+  const computer = useGLTF('./desktop_pc/scene.gltf');
 
-export default Computers
+  return (
+    <group>
+      <hemisphereLight
+        intensity={0.15}
+        groundColor="black"
+      />
+
+      <pointLight intensity={1} />
+      <primitive
+        object={computer.scene}
+        scale={0.75}
+        position={[0, -3.25, -1.5]}
+      />
+    </group>
+  );
+};
+
+const ComputersCanvas = () => {
+  return (
+    <Canvas
+      frameLoop="demand"
+      shadows
+      camera={{ position: [20, 3, 5], fov: 25 }}
+      gl={{ preserveDrawingBuffer: true }}>
+      <Suspense>
+        <OrbitControls
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+        />
+        <Computers />
+      </Suspense>
+      <Preload all />
+    </Canvas>
+  );
+};
+
+export default ComputersCanvas;
