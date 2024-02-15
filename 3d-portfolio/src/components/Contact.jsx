70d38a3669a402782db5abe765accf5170418c0a
Dima-Kutaini <deemaKutainy@gmail.com>
2024-02-15 13:37:51 +0100

    create Stars component

--- 3d-portfolio/src/components/Contact.jsx
+++ 3d-portfolio/src/components/Contact.jsx
@@ -1,14 +1,15 @@
 import React from 'react'; 
 import {useState, useRef} from "react"; 
 import {motion} from "framer-motion"; 
-import {emailjs} from "@emailjs/browser";
+import emailjs from "@emailjs/browser";
 import {styles}  from "../styles"; 
 import {EarthCanvas} from "./canvas";
 import {SectionWrapper} from "../hoc"; 
 import { slideIn } from '../utils/motion';
 
-
-
+ //template_ph9lgxs
+ // service_h44g47u
+ // DFYzlN-r1-IRa0uda     //key
 const Contact = () => {
   const fromRef= useRef(); 
   const [form, setForm]=useState({
@@ -18,9 +19,41 @@ const Contact = () => {
   }); 
   const [loading, setLoading]=useState(false); 
   const handleChange=(e)=>{
+    const {name,value}= e.target; 
+    setForm({...form, [name]:value})
 
   }; 
-  const handleSubmit=(e)=>{}
+  const handleSubmit=(e)=>{
+ e.preventDefault();
+ setLoading(true); 
+  emailjs.send(
+    'service_h44g47u',
+    'template_ph9lgxs',
+    {
+      from_name: form.name,
+      to_name: 'Dima',
+      from_email: form.email,
+      to_email: 'deemakutainy@gmail.com',
+      message: form.message,
+    },
+    'DFYzlN-r1-IRa0uda '
+  )
+  .then(()=>{
+    setLoading(false); 
+    alert("Thank you, I will get back to you as soon as possible"); 
+    setForm({
+      name:"",
+      email:"", 
+      message:"",
+    })
+
+  }, (error)=>{
+    setLoading(false)
+    // console.log(error); 
+    alert("Something went wrong!")
+  })
+
+  }
   return (
     <div className="xl:mt-12  xl:flex-row flex-col-reverse flex gap-10 overflow-hidden">
       <motion.div
