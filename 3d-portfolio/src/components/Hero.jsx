81194b8f0597baaa5ed2d5bf65c0ec2d9b2718a6
Dima-Kutaini <deemaKutainy@gmail.com>
2024-02-27 22:31:01 +0100

    fix img in Hero component

--- 3d-portfolio/src/components/Hero.jsx
+++ 3d-portfolio/src/components/Hero.jsx
@@ -14,7 +14,6 @@ const Hero = () => {
           <div className="w-5 h-5 rounded-full bg-[#915EFF]" />
           <div className="w-1 sm:h-80 h-40 violet-gradient" />
         </div>
-
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>
             Hi, I'm <span className="text-[#915EFF]">Dima </span>
@@ -23,18 +22,17 @@ const Hero = () => {
             I create user interfaces and <br className="sm:block hidden" />
             develop dynamic web applications
           </p>
-          <div className="mt-10 ml-20 text-center items-center justify-center xs:w-50 h-90">
+          <div className="mt-12 ml-20 text-center items-center justify-center xs:block hidden xs:w-50 h-90">
             <img
               // src="./desktop_pc/computer.png"
               src="https://raw.githubusercontent.com/BhavyaCodes/BhavyaCodes/master/.github/cat.gif"
+              // src="https://user-images.githubusercontent.com/74038190/213760718-ca064723-1c29-4b82-985c-aadc7f57c090.gif"
               alt="computer "
-              className="object-fill h-96 w-100  "
+              className="object-fill  "
             />
           </div>
         </div>
       </div>
-
-      {/* <ComputersCanvas />  */}
 
       <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
         <a href="#about">
@@ -48,7 +46,7 @@ const Hero = () => {
                 repeat: Infinity,
                 repeatType: 'loop',
               }}
-              className="w-3 h-3 rounded-full bg-secondary mb-1"
+              className="w-3 h-3 rounded-full bg-secondary mb-1 mt-2"
             />
           </div>
         </a>
